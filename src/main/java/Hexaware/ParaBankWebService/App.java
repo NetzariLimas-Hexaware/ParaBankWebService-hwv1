1922eeddfdc877789fb22a4a2d181e5b0ee6f305
Netzari gilberto limas rodriguez <2000082962@hexaware.com>
2023-03-09 00:02:04 -0600

    new files

---
+++ src/main/java/Hexaware/ParaBankWebService/App.java
@@ -0,0 +1,13 @@
+package Hexaware.ParaBankWebService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
