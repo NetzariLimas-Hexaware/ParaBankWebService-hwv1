69f92c264ae5258e888a1876b2c75ab6d8887d0c
Netzari gilberto limas rodriguez <2000082962@hexaware.com>
2023-03-13 12:20:06 -0600

    Se acabo del case study

--- src/test/java/Hexaware/HttpMethods/PetMethods.java
+++ src/test/java/Hexaware/HttpMethods/PetMethods.java
@@ -24,4 +24,32 @@ public class PetMethods {
 				.then().log().all().assertThat().statusCode(200)
 				.extract().response().asString();
 	}
+	
+	public String FindPetsByStatus(String status){
+		return given()
+				.log().all()
+				.header("Content-Type","application/json")
+				.queryParam("status", status)
+				.when()
+				.log().all()
+				.get("/pet/findByStatus")
+				.then()
+				.log().all()
+				.assertThat().statusCode(200)
+				.extract().response().asString();
+	}
+	
+	public String PlaceAnOrderForAPet(String petBody){
+		return given()
+				.log().all()
+				.header("Content-type", "application/json")
+				.body(petBody)
+				.when()
+				.log().all()
+				.post("/store/order")
+				.then()
+				.log().all()
+				.assertThat().statusCode(200)
+				.extract().response().asString();
+	}
 }
--- src/test/java/Hexaware/tests/GetTotNumAvaPets.java
+++ src/test/java/Hexaware/tests/GetTotNumAvaPets.java
@@ -1,7 +1,10 @@
 package Hexaware.tests;
 
 import Hexaware.TestComponents.BaseTest;
+import Hexaware.data.AddDelPetsData;
+import Hexaware.utils.JsonConverter;
 import io.restassured.filter.session.SessionFilter;
+import io.restassured.path.json.JsonPath;
 
 public class GetTotNumAvaPets extends BaseTest {
 
@@ -12,6 +15,35 @@ public class GetTotNumAvaPets extends BaseTest {
 			initializeWebServiceApp();
 			SessionFilter session = setSession();
 		//
+			
+		// Create a pet
+			String response = setPetMethods().AddNewPetToStore(AddDelPetsData.addPetBody());
+			JsonPath responseJson = JsonConverter.RawToJson(response);		
+			String petId = responseJson.getString("id");
+//			System.out.println(response);
+			
+		// Find the status of the pet added as available
+			response = setPetMethods().FindPetsByStatus("available");
+			responseJson = JsonConverter.RawToJson(response);
+			
+			int totAvailablePets = responseJson.getInt("size()");
+			System.out.println("Total pets: " + totAvailablePets);
+			
+		// Delete the pet recently added
+			
+			response = setPetMethods().DeletePet(petId, session);
+			responseJson = JsonConverter.RawToJson(response);
+//			System.out.println(responseJson);
+			
+			// Check again for available pets
+			response = setPetMethods().FindPetsByStatus("available");
+			responseJson = JsonConverter.RawToJson(response);
+			int remTotAvailablePets = responseJson.getInt("size()");
+			System.out.println("Remaining Pets:" + remTotAvailablePets);
+			
+			int dif = totAvailablePets - remTotAvailablePets;
+			if(dif == 1)
+				System.out.println("Second scenario done.");
 	}
 
 }
